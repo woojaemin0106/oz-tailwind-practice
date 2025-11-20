c49b1041c440acfb5d8a6df7d532bb3f524bd067
woojaemin0106 <dnwoals19@naver.com>
2025-11-20 13:33:46 +0900

    테일윈드 css 연습하기

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,19 @@
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
+    <div className="flex flex-col items-start gap-1">
+      <img
+        src={content.img}
+        alt=""
+        className="w-[300px] rounded-[10px] mb-[3px] min-h-[225px]"
+      />
+
+      <span className="text-[12px] text-[#d7fa00] [border:1pxsolid#d7fa00] p-[4px_5px] rounded-[3px]">
+        모집중
+      </span>
+
+      <div className="text-[18px] font-semibold">{content.title}</div>
+
+      <p className="text-[12px] text-gray-400">{content.subtitle}</p>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="flex justify-between items-center px-5 py-2 bg-black">
+      <h1 className="text-white font-bold text-lg">OZ코딩스쿨</h1>
+
+      <ul className="flex items-center justify-center gap-5">
+        <li className="list-none text-white text-sm font-normal">로그인</li>
+        <li className="list-none text-white text-sm font-normal">회원가입</li>
+        <li className="list-none text-white text-sm font-normal">내클래스</li>
       </ul>
     </header>
   );
